e5d28ed5f206d93978dfae6d86d1c6787fcc031b
trandung <trandung164.ptit@gmail.com>
2020-12-13 16:43:49 +0700

    remove fake

--- src/main/java/com/ptit/trandung/repository/MedicalHistoryRepository.java
+++ src/main/java/com/ptit/trandung/repository/MedicalHistoryRepository.java
@@ -2,8 +2,12 @@ package com.ptit.trandung.repository;
 
 import com.ptit.trandung.domain.MedicalHistory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data  repository for the MedicalHistory entity.
@@ -11,4 +15,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
+
+    Page<MedicalHistory> findAllByPatientId(Long id, Pageable pageable);
 }
--- src/main/java/com/ptit/trandung/service/impl/MedicalHistoryServiceImpl.java
+++ src/main/java/com/ptit/trandung/service/impl/MedicalHistoryServiceImpl.java
@@ -1,6 +1,10 @@
 package com.ptit.trandung.service.impl;
 
+import com.ptit.trandung.domain.Doctor;
+import com.ptit.trandung.domain.Nurse;
 import com.ptit.trandung.domain.enumeration.MedicalHistoryStatus;
+import com.ptit.trandung.repository.DoctorRepository;
+import com.ptit.trandung.repository.NurseRepository;
 import com.ptit.trandung.service.MedicalHistoryService;
 import com.ptit.trandung.domain.MedicalHistory;
 import com.ptit.trandung.repository.MedicalHistoryRepository;
@@ -9,11 +13,16 @@ import com.ptit.trandung.service.mapper.MedicalHistoryMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -29,6 +38,12 @@ public class MedicalHistoryServiceImpl implements MedicalHistoryService {
 
     private final MedicalHistoryMapper medicalHistoryMapper;
 
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    @Autowired
+    NurseRepository nurseRepository;
+
     public MedicalHistoryServiceImpl(MedicalHistoryRepository medicalHistoryRepository, MedicalHistoryMapper medicalHistoryMapper) {
         this.medicalHistoryRepository = medicalHistoryRepository;
         this.medicalHistoryMapper = medicalHistoryMapper;
@@ -38,7 +53,15 @@ public class MedicalHistoryServiceImpl implements MedicalHistoryService {
     public MedicalHistoryDTO save(MedicalHistoryDTO medicalHistoryDTO) {
         log.debug("Request to save MedicalHistory : {}", medicalHistoryDTO);
         MedicalHistory medicalHistory = medicalHistoryMapper.toEntity(medicalHistoryDTO);
-//        if (MedicalHistoryStatus.DONE.equals(medicalHistory.))
+        if (MedicalHistoryStatus.DONE.equals(medicalHistory.getStatus()) && !medicalHistory.isIsPaid()){
+            Doctor doctor = medicalHistory.getDoctor();
+            doctor.setSalary(doctor.getSalary()+1000000);
+            doctorRepository.save(doctor);
+
+            Nurse nurse = medicalHistory.getNurse();
+            nurse.setSalary(nurse.getSalary()+200000);
+            nurseRepository.save(nurse);
+        }
         medicalHistory = medicalHistoryRepository.save(medicalHistory);
         return medicalHistoryMapper.toDto(medicalHistory);
     }
@@ -64,5 +87,27 @@ public class MedicalHistoryServiceImpl implements MedicalHistoryService {
     public void delete(Long id) {
         log.debug("Request to delete MedicalHistory : {}", id);
         medicalHistoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<MedicalHistoryDTO> getAllByPatientId(Long userId, Pageable pageable) {
+        return medicalHistoryRepository.findAllByPatientId(userId,pageable)
+            .map(medicalHistoryMapper::toDto);
+    }
+
+    // ngày đầu của mỗi tháng
+    @Scheduled(cron = "0 0 0 1 1/1 ?")
+    public void calculatedSalary() {
+        List<Doctor> doctors = doctorRepository.findAll();
+        doctors.forEach(doctor -> {
+            doctor.setSalary(7000000l);
+        });
+        doctorRepository.saveAll(doctors);
+
+        List<Nurse> nurses = nurseRepository.findAll();
+        nurses.forEach(nurse -> {
+            nurse.setSalary(7000000l);
+        });
+        nurseRepository.saveAll(nurses);
     }
 }
